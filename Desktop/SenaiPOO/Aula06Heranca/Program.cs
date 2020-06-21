c0c1efeb579aba0ae9a33ba4369023340588c28d
Amanda Nascimento <amandinha.fia87@gmail.com>
2020-06-21 14:28:07 -0300

    Aula06 finalizada

---
+++ Desktop/SenaiPOO/Aula06Heranca/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Aula06Heranca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CPF cpf = new CPF();
+            cpf.nome = "Ana";
+            
+            System.Console.WriteLine(cpf.Saudar());
+        }
+    }
+}
