5890ac0b7bc115a4f00bc11f1cbcf7f9652a6167
Amanda Nascimento <amandinha.fia87@gmail.com>
2020-06-21 19:55:18 -0300

    Aula20 finalizada

---
+++ Desktop/SenaiPOO/Aula 20 - fixacao3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Aula_20___fixacao3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente ana = new Cliente();
+            ana.NomeCliente = "Ana";
+            ana.Endereco = "Avenida D. Pedro, 123";
+            Console.WriteLine(ana.MostrarDados());
+
+            Restaurante madero = new Restaurante();
+            madero.NomeRestaurante = "Madero";
+            madero.Endereco = "Avenida Ricardo Jafet";
+            System.Console.WriteLine(madero.MostrarDados());
+
+            Pedido pedido1 = new Pedido();
+            pedido1.Cliente = ana.NomeCliente;
+            pedido1.Restaurante = madero.NomeRestaurante;
+            Console.WriteLine(pedido1.EntregarPedido());
+        }
+    }
+}
