c41b3112100e7dbe66430034734ec22fc428b9d1
Amanda Nascimento <amandinha.fia87@gmail.com>
2020-06-25 10:21:31 -0300

    Aula 25 finalizada

---
+++ Desktop/SenaiPOO/Aula25Recursividade/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Aula25Recursividade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Recursividade rec = new Recursividade();
+            rec.GerarSeqenciaFibonacci(0,1,20);
+            rec.GerarFatorial(5);
+        }
+    }
+}
