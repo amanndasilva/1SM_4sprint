dc26b5e578d8814c7aced97172ee294fda4765d9
Amanda Nascimento <amandinha.fia87@gmail.com>
2020-06-26 10:52:25 -0300

    Aula 26 finalizada

---
+++ Desktop/SenaiPOO/Aula26Interfaces/Carrinho.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+
+namespace Aula26Interfaces
+{
+    public class Carrinho : ICarrinho
+    {
+        public float ValorTotal { get; set; }
+        List <Produto> carrinho = new List<Produto>();
+        public void Alterar(int _cod, Produto produto)
+        {
+            carrinho.Find(a => a.Codigo == _cod).Nome = produto.Nome;
+            carrinho.Find(b => b.Codigo ==  _cod).Preco = produto.Preco;
+        }
+
+        public void Excluir(Produto _produto)
+        {
+            carrinho.Remove(_produto);
+        }
+
+        public void Incluir(Produto _produto)
+        {
+            carrinho.Add(_produto);
+        }
+
+        public void Ler()
+        {
+            foreach(Produto p in carrinho){
+                System.Console.WriteLine($"R$ {p.Preco} \nNome: {p.Nome}");
+            }
+        }
+
+        public void SomarTotal()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            foreach(Produto p in carrinho)
+            {
+                ValorTotal += p.Preco;
+            }
+            System.Console.WriteLine($"\nValor total dos itens: R$ {ValorTotal}.");
+            Console.ResetColor();
+        }
+    }
+}
