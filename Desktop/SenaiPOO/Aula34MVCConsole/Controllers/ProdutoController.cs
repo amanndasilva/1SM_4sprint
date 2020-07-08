c75cda0aa24afc40c184f4128f47f497b573aeda
Amanda Nascimento <amandinha.fia87@gmail.com>
2020-07-08 11:14:22 -0300

    Aula 34 finalizada

---
+++ Desktop/SenaiPOO/Aula34MVCConsole/Controllers/ProdutoController.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using Aula34MVCConsole.Models;
+using Aula34MVCConsole.Views;
+
+namespace Aula34MVCConsole.Controllers
+{
+    public class ProdutoController
+    {
+        Produto produtoModel = new Produto();
+        ProdutoView produtoView = new ProdutoView();
+
+        public void Listar()
+        {
+            produtoView.MostrarNoConsle(produtoModel.Ler());
+        }
+
+        public void Buscar(string _preco)
+        {
+            List<Produto> busca = produtoModel.Ler().FindAll
+            (x => x.Preco == float.Parse(_preco));
+            
+            produtoView.MostrarNoConsle(busca);
+        }
+    }
+}
---
+++ Desktop/SenaiPOO/Aula34MVCConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Aula34MVCConsole.Controllers;
+
+namespace Aula34MVCConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProdutoController prod = new ProdutoController();
+            prod.Listar();
+            //prod.Buscar("400");
+        }
+    }
+}
